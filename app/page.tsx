ce91c2e397d64180da5039d54d4ce0bbe92acc3a
JohnKHancock <jkhancock@gmail.com>
2026-01-05 23:50:12 -0500

    Fixed 13th Deployment Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Header } from "./components/header";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-950">
